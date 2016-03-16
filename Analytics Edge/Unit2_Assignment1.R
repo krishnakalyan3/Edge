d6259b5afc90deaa0ca2f7cda7c736546b78b48b
krishnakalyan3 <krishnakalyan3@gmail.com>
2016-03-16 10:54:26 +0100

    Commit Date Wed Mar 16 10:54:24 CET 2016

--- Analytics Edge/Unit2_Assignment1.R
+++ Analytics Edge/Unit2_Assignment1.R
@@ -1,3 +1,5 @@
+getwd()
+setwd("/Users/krishna/MOOC/Edge/Data")
 elantra = read.csv("elantra.csv")
 head(elantra)
 str(elantra)
@@ -16,3 +18,30 @@ summary(model2)
 model3 = lm(ElantraSales ~ as.factor(Month) + Unemployment + CPI_all + CPI_energy + Queries, data = ElantraTrain)
 summary(model3)
 cor(ElantraTrain)
+
+ElantraTrain.factor = cbind(ElantraTrain,as.factor(ElantraTrain$Year))
+head(ElantraTrain.factor)
+
+model4 = lm(ElantraSales ~ as.factor(Month) + Unemployment + CPI_all + CPI_energy + Queries, data = ElantraTrain)
+summary(model4)
+
+model5 = lm(ElantraSales ~ as.factor(Month) + Unemployment + CPI_all + CPI_energy , data = ElantraTrain)
+summary(model5)
+
+
+# 190757747
+mean(ElantraTrain$ElantraSales)
+predict(model5,ElantraTest)
+
+#You can compute the SST as the sum of the squared differences 
+#between ElantraSales in the testing set and the mean of ElantraSales 
+SST = sum((ElantraTest$ElantraSales  - mean(ElantraTrain$ElantraSales))^2)
+SSE= sum((ElantraTest$ElantraSales - predict(model5,ElantraTest))^2)
+
+Rsq = 1- SSE/SST
+Rsq
+
+abserror = abs(ElantraTest$ElantraSales - predict(model5,ElantraTest))
+indexmax =match(max(abserror),abserror)
+
+ElantraTest[indexmax,]
