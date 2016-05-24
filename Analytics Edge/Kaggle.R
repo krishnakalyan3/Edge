0dd20dacf949fe39dcdee3e98e4ad75b5e2b8a79
krishnakalyan3 <krishnakalyan3@gmail.com>
2016-05-24 23:55:39 +0200

    Commit Date Tue May 24 23:55:36 CEST 2016

---
+++ Analytics Edge/Kaggle.R
@@ -0,0 +1,88 @@
+getwd()
+library(randomForest)
+rm(list=ls())
+
+library(caret)
+library(e1071)
+install.packages("VIM")
+setwd("/Users/krishna/MOOC/Edge/Kaggle")
+train1 = read.csv("train2016.csv")
+test = read.csv("test2016.csv")
+
+names(train1)
+
+
+# Fix YOB
+train1$YOB[is.na(train1$YOB)] = 1980
+test$YOB[is.na(test$YOB)] = 1980
+
+#YOB 
+# Mean 1980
+# Median 1983
+
+#train1[,7:100] =sapply(train1[,7:100], as.numeric)
+#train1$YOB = as.factor(train1$YOB)
+#test$YOB = as.factor(test$YOB)
+#train1 = train1[, !(colnames(train1) %in% c("USER_ID"))]
+
+
+# Data Pre=Processig
+#library(corrplot)
+#summary(train1)
+#str(train1)
+# Divide into train , validation , test set
+set.seed(144)
+library(caTools)
+split = sample.split(train1$Party, SplitRatio = 0.7)
+train = subset(train1, split == TRUE)
+validation = subset(train1, split == FALSE)
+
+#str(train)
+#summary(train)
+
+#dim(train)
+#dim(validation)
+
+# NAs in my dataset
+sum(is.na(test))
+
+#md.pattern(train)
+summary(train)
+
+model1 = randomForest(Party ~ ., train)
+
+#summary(model1)
+#plot(model1, log="y")
+#varImpPlot(model1)
+# Accuracy and F1 Score
+predictval = predict(model1, validation)
+cm = table(validation$Party,predictval)
+TN = cm[1,1]
+TP = cm[2,2]
+FN = cm[2,1]
+FP = cm[1,2]
+Acc = (TP + TN)/sum(cm) 
+Acc
+
+#test1 = test[, !(colnames(test) %in% c("USER_ID"))]
+# Writing Output
+predictTest = predict(model1, test)
+op =cbind.data.frame(test$USER_ID,predictTest)
+colnames(op) <-  c("USER_ID","Predictions")
+summary(op)
+head(op)
+write.table(op,"output.csv", row.names=FALSE,sep=",",quote=F)
+?write.table
+
+
+
+# Models Tried
+model1 <- randomForest(Party ~ . -USER_ID , data = trainimp,  keep.forest=TRUE,
+                       importance=TRUE)
+control <- trainControl(method="repeatedcv", number=10, repeats=3)
+tunegrid <- expand.grid(.mtry=c(1:15))
+metric <- "Accuracy"
+rf_gridsearch <- train(Party~., data=train, method="rf", metric=metric, tuneGrid=tunegrid, trControl=control)
+print(rf_gridsearch)
+plot(rf_gridsearch)
+
---
+++ Kaggle/PreRecall.R
@@ -0,0 +1,20 @@
+measurePrecisionRecall <- function(predict, actual_labels){
+  precision <- sum(predict & actual_labels) / sum(predict)
+  recall <- sum(predict & actual_labels) / sum(actual_labels)
+  fmeasure <- 2 * precision * recall / (precision + recall)
+  
+  cat('precision:  ')
+  cat(precision * 100)
+  cat('%')
+  cat('\n')
+  
+  cat('recall:     ')
+  cat(recall * 100)
+  cat('%')
+  cat('\n')
+  
+  cat('f-measure:  ')
+  cat(fmeasure * 100)
+  cat('%')
+  cat('\n')
+}
